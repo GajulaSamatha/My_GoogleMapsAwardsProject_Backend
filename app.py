34d6dcd9105dbff6c943a729ba939a0331808d7d
Samatha Gajula <samathagajulaofficial@gmail.com>
2025-07-28 05:49:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def get_fuel_price(city):
 
     return jsonify(sample_data)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
